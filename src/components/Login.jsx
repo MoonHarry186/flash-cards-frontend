924f0471ec0e90bdea64f19de40cfa80a55c0c3f
phucb <harrymoon1862@gmail.com>
2024-10-01 23:24:46 +0700

    first commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../layout/Layout'
+const Login = () => {
+  return (
+    <Layout>
+      <div className='max-w-screen-xl mx-auto'>
+        <h3>Login</h3>
+      </div>
+    </Layout>
+  )
+}
+
+export default Login
